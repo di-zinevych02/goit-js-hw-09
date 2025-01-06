d01035dfeb8a93f4ec15e28f742123dcf9da17a9
Diana Zinevych <dianazinevych02@gmail.com>
2025-01-06 14:41:30 +0200

    Update 2-form.js
    add comments for myself

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,6 +3,8 @@ let formData = {
   email: '',
   message: '',
 };
+// Дістали із колекції feedbackFormEl.elements два елементи з іменами email та message.Створили для них окремі змінні email та message. Тобто:email тепер посилається на <input type="email" name="email">. message посилається на <textarea name="message"></textarea>.
+
 const { email, message } = feedbackFormEl.elements;
 
 const funFormData = () => {
@@ -22,9 +24,12 @@ const funFormData = () => {
 };
 funFormData();
 const funFormInput = event => {
-  const formEl = event.target;
+ // Eлемент, на якому сталася подія
+  const formEl = event.target; 
+  // Отримуємо значення введеного email
   const inputValue = formEl.value;
   const inputName = formEl.name;
+  // Додає або оновлює властивість в об'єкті formData з динамічною назвою властивості, взятою з inputName, і присвоює їй значення inputValue.
   formData[inputName] = inputValue;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 };
@@ -41,6 +46,7 @@ const btnFormSubmit = event => {
 
   formEl.reset();
   localStorage.removeItem('feedback-form-state');
+  // Об'єкт formData, який зберігав значення форми, очищається.
   formData = {};
 };
 feedbackFormEl.addEventListener('input', funFormInput);
